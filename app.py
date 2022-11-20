f323e33ba176327a876e5d4a87f2f68c1ac8d735
tando <tandonsaurabh07@gmail.com>
2022-11-20 19:16:08 +0530

    opp

--- app.py
+++ app.py
@@ -328,5 +328,6 @@ class Youtube:
 
     if __name__ == "__main__":
         #app.run(host='127.0.0.1', port=8002, debug=True)
-        app.run(debug=True)
+        app.run(host="0.0.0.0",debug=True,port=os.environ['PORT'])
+
 
